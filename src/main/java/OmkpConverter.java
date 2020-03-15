b16dab932a9868428a2f58f356d2bfd834356c7f
AlehKryvets <mrceheka@gmail.com>
2020-03-15 23:07:45 +0300

    TEST

--- src/main/java/OmkpConverter.java
+++ src/main/java/OmkpConverter.java
@@ -16,9 +16,9 @@ public class OmkpConverter {
         int classifierattributeid = 860;
         int classifieritemid = 2299372;
 
-        FileWriter writer = new FileWriter("/home/user/Downloads/Project/OMKP.sql");
+        FileWriter writer = new FileWriter("f:\\OMKP.sql");
 
-        FileInputStream stream = new FileInputStream(new File("/home/user/Downloads/Project/MKPO.xlsx"));
+        FileInputStream stream = new FileInputStream(new File("f:\\MKPO.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(stream);
         XSSFSheet sheet = workbook.getSheetAt(0);
 
@@ -29,8 +29,11 @@ public class OmkpConverter {
         writer.write("insert into classifier (classifierid,codeen,coderu,categoryid,status) values ("+classifierid+", 'МКПО', 'МКПО', 1,0);\n");
         writer.write("\n");
 
-        String attributes_rus = "Примечание";
-        writer.write("insert into classifierattribute (classifierattributeid,classifierid,type,name,nameen,attributeorder) values ("+(classifierattributeid)+", "+classifierid+", 0, '"+attributes_rus+"','"+attributes_rus+"',"+(1)+");\n");
+        String[] attributes_rus = {"Примечание","Тип", "Расшифровка"};
+        for (int i = 0; i < 3; i++) {
+            writer.write("insert into classifierattribute (classifierattributeid,classifierid,type,name,nameen,attributeorder) values ("+(classifierattributeid)+", "+classifierid+", 0, '"+attributes_rus[i]+"','"+attributes_rus[i]+"',"+(1)+");\n");
+        }
+
 
 
         Iterator<Row> rowIterator = sheet.iterator();
@@ -64,9 +67,9 @@ public class OmkpConverter {
             } else {
                 writer.write("insert into classifieritem (classifieritemid,classifierid,parentclassifieritemid,name,code) values ("+classifieritemid+", "+classifierid+", "+parents.get(parentId)+", '"+row.getCell(3).getStringCellValue().replaceAll("'","''")+"', '"+row.getCell(id).getStringCellValue()+"');\n");
             }
-            System.out.println("insert into classifieritem (classifieritemid,classifierid,parentclassifieritemid,name,code) values ("+classifieritemid+", "+classifierid+", "+parents.get(parentId)+", '"+row.getCell(3).getStringCellValue().replaceAll("'","''")+"', '"+row.getCell(id).getStringCellValue()+"')");
-
-            writer.write("insert into classifieritemattributevalue (classifierattributeid,classifieritemid,textvalue) values ("+(classifierattributeid)+", "+classifieritemid+", "+((row.getCell(5) == null) ? "null" : "'"+row.getCell(5).getStringCellValue().replaceAll("'","''")+"'")+");\n");
+            for (int i = 0; i < 0; i++) {
+                writer.write("insert into classifieritemattributevalue (classifierattributeid,classifieritemid,textvalue) values (" + (classifierattributeid) + ", " + classifieritemid + ", " + ((row.getCell(i+5) == null) ? "null" : "'" + row.getCell(i+5).getStringCellValue().replaceAll("'", "''") + "'") + ");\n");
+            }
             writer.write("\n");
 
             classifieritemid++;
