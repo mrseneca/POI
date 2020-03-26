c9f04a7730ae46107caf644dee6aaaab67fa9cfb
AlehKryvets <mrceheka@gmail.com>
2020-03-26 15:14:03 +0300

    Childs

--- src/main/java/XmlToExcelCpcConverter.java
+++ src/main/java/XmlToExcelCpcConverter.java
@@ -1,15 +1,15 @@
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+        import org.w3c.dom.Element;
+        import org.w3c.dom.Node;
+        import org.w3c.dom.NodeList;
+        import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+        import javax.xml.parsers.DocumentBuilder;
+        import javax.xml.parsers.DocumentBuilderFactory;
+        import javax.xml.parsers.ParserConfigurationException;
+        import java.io.File;
+        import java.io.IOException;
+        import java.util.ArrayList;
 
 public class XmlToExcelCpcConverter {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
@@ -19,15 +19,11 @@ public class XmlToExcelCpcConverter {
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(xmlFile);
         doc.getDocumentElement().normalize();
-<<<<<<< HEAD
 
         NodeList classificationItems = doc.getElementsByTagName("classification-item");
         for (int i = 0; i < classificationItems.getLength(); i++) {
             NodeList itemChildren = classificationItems.item(0).getChildNodes();
-            
+
         }
-=======
-        
->>>>>>> Test-child-for
     }
 }
