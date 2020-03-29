df4097bac14afb45236a7775ba622cf0169f53ab
AlehKryvets <mrceheka@gmail.com>
2020-03-29 21:47:28 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit d91bf66cc3747c644db0cdc6128bf4aea88ffbf3.

--- src/main/java/CPCConverter.java
+++ src/main/java/CPCConverter.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import org.apache.poi.ss.usermodel.CellType;
-=======
->>>>>>> master
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -16,17 +13,10 @@ import java.util.Iterator;
 
 public class CPCConverter {
     public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
         int objectid = 59679;
         int classifierid = 59679;
         int classifierattributeid = 978;
         int classifieritemid = 2507407;
-=======
-        int objectid = 59647;
-        int classifierid = 59647;
-        int classifierattributeid = 938;
-        int classifieritemid = 2447114;
->>>>>>> master
 
         FileWriter writer = new FileWriter("f:\\CPC.sql");
 
@@ -34,7 +24,6 @@ public class CPCConverter {
         writer.write("insert into object (objectid,name,categoryid,isdeleted) values ("+objectid+",'Cooperative Patent Classification', 1, false);\n");
         writer.write("\n");
 
-<<<<<<< HEAD
         writer.write("insert into classifier (classifierid,codeen,coderu,categoryid,status) values ("+classifierid+", '(CPC)', '(CPC)', 1,0);\n");
         writer.write("\n");
 
@@ -43,14 +32,6 @@ public class CPCConverter {
         for (int i = 0; i < 4; i++) {
             if (i==1) {ctype=1;} else {ctype=0;}
             writer.write("insert into classifierattribute (classifierattributeid,classifierid,type,name,nameen,attributeorder) values ("+(classifierattributeid+i)+", "+classifierid+", "+ctype+", '"+attributes[i]+"','"+attributes[i]+"',"+(i+1)+");\n");
-=======
-        writer.write("insert into classifier (classifierid,codeen,coderu,categoryid,status) values ("+classifierid+", 'CPC', 'CPC', 1,0);\n");
-        writer.write("\n");
-
-        String[] attributes = {"Уровень","Примечание","Предупреждение"};
-        for (int i = 0; i < 3; i++) {
-            writer.write("insert into classifierattribute (classifierattributeid,classifierid,type,name,nameen,attributeorder) values ("+(classifierattributeid+i)+", "+classifierid+", 0, '"+attributes[i]+"','"+attributes[i]+"',"+(i+1)+");\n");
->>>>>>> master
         }
         writer.write("\n");
         writer.close();
@@ -71,20 +52,13 @@ public class CPCConverter {
             Row row;
             HashMap<String, Integer> parents = new HashMap();
             String parentId;
-<<<<<<< HEAD
             String cellText;
             String cellType;
-=======
->>>>>>> master
 
             while (rowIterator.hasNext()) {
                 row = rowIterator.next();
 
-<<<<<<< HEAD
                 parents.put(row.getCell(4).getStringCellValue(), classifieritemid);
-=======
-                parents.put(row.getCell(0).getStringCellValue(), classifieritemid);
->>>>>>> master
                 if(row.getCell(1) ==null) {
                     parentId = "null";
                 } else {
@@ -93,15 +67,10 @@ public class CPCConverter {
 
 
                 writer.write("insert into classifieritem (classifieritemid,classifierid,parentclassifieritemid,name,code) values (" + classifieritemid + ", " + classifierid + ","+parents.get(parentId)+", '" + row.getCell(2).getStringCellValue().replaceAll("'", "''") + "', '" + row.getCell(0).getStringCellValue() + "');\n");
-<<<<<<< HEAD
                 for (int i = 0; i < 4; i++) {
                     if (i==1) {cellType="numbervalue";} else {cellType="textvalue";}
                     cellText = (row.getCell(i+4)==null) ? (i==1 ? "null" : "''") : (i != 1 ? "'"+row.getCell(i+4).getStringCellValue().replaceAll("'","''")+"'" : row.getCell(i+4).getStringCellValue().replaceAll("'","''"));
                     writer.write("insert into classifieritemattributevalue (classifierattributeid,classifieritemid,"+cellType+") values (" + (classifierattributeid + i) + ", " + classifieritemid + ", " + cellText + ");\n");
-=======
-                for (int i = 0; i < 3; i++) {
-                    writer.write("insert into classifieritemattributevalue (classifierattributeid,classifieritemid,textvalue) values (" + (classifierattributeid + i) + ", " + classifieritemid + ", " + ((row.getCell(i + 5) == null) ? "null" : "'" + row.getCell(i + 5).getStringCellValue().replaceAll("'", "''") + "'") + ");\n");
->>>>>>> master
                 }
                 writer.write("\n");
 
